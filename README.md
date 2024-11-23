a0a69dcff2338d746a7610a3a89c65f64c10be4f
Anwar Rahim <anwargithub@gmail.com>
2024-11-23 12:26:51 +0500

    adding new things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pumpkins-meme-picker
+
+<a href="https://anwarrahim.com/shoping-card-parentElement"> project link</a>
--- index.js
+++ index.js
@@ -2,17 +2,30 @@ import {catsData} from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', changeRadioColor)
+
+function changeRadioColor(e){
+    
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios ){
+        radio.classList.remove('highlight')
+    }
+}
+
 
 function getEmotionsArray(cats){
     const emotionsArray= []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if(!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+            
         }
     }
     return emotionsArray
 }
-
 
 
 function renderEmotionsRadios(cats){
